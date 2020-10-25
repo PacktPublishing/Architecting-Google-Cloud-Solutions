d971691e8920cc41e0f61fd883ec2ded5b39b8cc
Victor Dantas <vmehmeri@hotmail.com>
2020-10-25 09:46:36 +0100

    Fixed crontab step in setup script

--- ch4/autoscaling-mig/setup.sh
+++ ch4/autoscaling-mig/setup.sh
@@ -4,5 +4,4 @@ mkdir /var/app
 cp index.html.template /var/app/
 cp start_webserver.sh /var/app/
 
-#crontab -e
-@reboot /var/app/start_webserver.sh
+(crontab -l; echo "@reboot /var/app/start_webserver.sh") | crontab -
