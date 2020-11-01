a914b1cc27a18ef534d07f2f404b62534e9b62c8
Victor Dantas <vmehmeri@hotmail.com>
2020-11-01 16:41:24 +0100

    Fixed setup script for autoscaling-mig activity

--- ch4/autoscaling-mig/setup.sh
+++ ch4/autoscaling-mig/setup.sh
@@ -3,5 +3,6 @@
 mkdir /var/app
 cp index.html.template /var/app/
 cp start_webserver.sh /var/app/
+chmod +x /var/app/start_webserver.sh
 
 (crontab -l; echo "@reboot /var/app/start_webserver.sh") | crontab -
