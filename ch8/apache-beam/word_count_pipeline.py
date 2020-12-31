14e0be4af6a42dbffd62ba69a7ab4d554940c9b0
Victor Dantas <vmehmeri@hotmail.com>
2020-12-31 15:02:35 +0100

    Updated comments on ch8 files

--- ch8/apache-beam/word_count_pipeline.py
+++ ch8/apache-beam/word_count_pipeline.py
@@ -9,7 +9,7 @@ def word_map(line, words):
          yield (word,1)
 
 if __name__ == '__main__':
-   parser = argparse.ArgumentParser(description='Find the most used word')
+   parser = argparse.ArgumentParser(description='Find how often words appear at least once in lines')
    parser.add_argument('--output_prefix', default='tmp/output', help='Output prefix')
    parser.add_argument('--input', default='Frankenstein.txt', help='Input file')
    parser.add_argument('--words', default='Frankenstein,Margaret', help='Comma-separated list of words to count')
@@ -21,10 +21,10 @@ if __name__ == '__main__':
    output_prefix = options.output_prefix
    words = options.words.split(',')
 
-   # find most used packages
+   # Pipeline definition
    (p
       | 'ReadFile' >> beam.io.ReadFromText(input)
-      ## beam.FlatMap is equivalent to ParDo in Python
+        ## beam.FlatMap is equivalent to ParDo in Python
       | 'GetWords' >> beam.FlatMap(lambda line: word_map(line, words))
       | 'TotalUse' >> beam.CombinePerKey(sum)
       | 'write' >> beam.io.WriteToText(output_prefix)
